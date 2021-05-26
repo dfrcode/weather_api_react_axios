de0a2fb82acc12e1a9e64e13a6765f73d64a00bd
Denis Frolov <denisfrolov.coder@gmail.com>
2021-05-27 00:19:20 +0300

    test changed city/delete data in state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -11,50 +11,48 @@ class App extends Component {
     this.state = {
       city: "Краснодар",
       // data api
-      data: {
-        base: null,
-        clouds: {
-          all: null,
-        },
-        coord: {
-          lat: null,
-          lon: null,
-        },
-        dt: null,
+      base: null,
+      clouds: {
+        all: null,
+      },
+      coord: {
+        lat: null,
+        lon: null,
+      },
+      dt: null,
+      id: null,
+      main: {
+        feels_like: null,
+        grnd_level: null,
+        humidity: null,
+        pressure: null,
+        sea_level: null,
+        temp: null,
+        temp_max: null,
+        temp_min: null,
+      },
+      name: null,
+      sys: {
+        country: null,
         id: null,
-        main: {
-          feels_like: null,
-          grnd_level: null,
-          humidity: null,
-          pressure: null,
-          sea_level: null,
-          temp: null,
-          temp_max: null,
-          temp_min: null,
-        },
-        name: null,
-        sys: {
-          country: null,
+        sunrise: null,
+        sunset: null,
+        type: null,
+      },
+      timezone: null,
+      visibility: null,
+      weather: [
+        {
+          description: null,
+          icon: null,
           id: null,
-          sunrise: null,
-          sunset: null,
-          type: null,
+          main: null,
         },
-        timezone: null,
-        visibility: null,
-        weather: [
-          {
-            description: null,
-            icon: null,
-            id: null,
-            main: null,
-          },
-        ],
-        wind: {
-          deg: null,
-          gust: null,
-          speed: null,
-        },
+      ],
+      wind: {
+        deg: null,
+        gust: null,
+        speed: null,
       },
     };
 
@@ -65,14 +63,71 @@ class App extends Component {
     const res = await axios.get(
       `${this.apiPath}?q=${city}&appid=${process.env.REACT_APP_API_KEY}&lang=${this.apiLang}`
     );
-    console.log(res.data);
+    this.setState((state) => {
+      return {
+        base: res.data.base,
+        clouds: {
+          all: res.data.clouds.all,
+        },
+        coord: {
+          lat: res.data.coord.lat,
+          lon: res.data.coord.lon,
+        },
+        dt: res.data.dt,
+        id: res.data.id,
+        main: {
+          feels_like: res.data.main.feels_like,
+          grnd_level: res.data.main.grnd_level,
+          humidity: res.data.main.humidity,
+          pressure: res.data.main.pressure,
+          sea_level: res.data.main.sea_level,
+          temp: res.data.main.temp,
+          temp_max: res.data.main.temp_max,
+          temp_min: res.data.main.temp_min,
+        },
+        name: res.data.name,
+        sys: {
+          country: res.data.sys.country,
+          id: res.data.sys.id,
+          sunrise: res.data.sys.sunrise,
+          sunset: res.data.sys.sunset,
+          type: res.data.sys.type,
+        },
+        timezone: res.data.timezone,
+        visibility: res.data.visibility,
+        weather: [
+          {
+            description: res.data.weather.description,
+            icon: res.data.weather.icon,
+            id: res.data.weather.id,
+            main: res.data.weather.main,
+          },
+        ],
+        wind: {
+          deg: res.data.wind.deg,
+          gust: res.data.wind.gust,
+          speed: res.data.wind.speed,
+        },
+      };
+    });
   }
+
+  newCityWeather = (e) => {
+    this.setState({
+      city: e.target.value,
+    });
+    this.getResoures(this.state.city);
+  };
 
   render() {
     return (
-      <div>
-        <h1>App</h1>
-      </div>
+      <>
+        <input
+          type="text"
+          value={this.state.city}
+          onChange={this.newCityWeather}
+        />
+      </>
     );
   }
 }
