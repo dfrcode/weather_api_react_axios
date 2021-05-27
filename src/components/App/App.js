0186a356a370bf752e20bfa73ce0adbd890a5e7b
Denis Frolov <denisfrolov.coder@gmail.com>
2021-05-27 13:19:53 +0300

    added changed degrees/added styles app/added output info

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
 import axios from "axios";
 
+import "./App.css";
+
 import { cities } from "../../utils/cities";
+
+import { FaLocationArrow } from "react-icons/fa";
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -10,10 +15,13 @@ class App extends Component {
     this.apiLang = "ru";
 
     this.state = {
+      celsius: true,
+      fahrenheit: false,
       localCity: "",
       localCityBool: false,
       city: "Краснодар",
       newCity: false,
+      changeCityBtn: false,
 
       // data api
       base: null,
@@ -46,14 +54,12 @@ class App extends Component {
       },
       timezone: null,
       visibility: null,
-      weather: [
-        {
-          description: null,
-          icon: null,
-          id: null,
-          main: null,
-        },
-      ],
+      weather: {
+        description: null,
+        icon: null,
+        id: null,
+        main: null,
+      },
       wind: {
         deg: null,
         gust: null,
@@ -61,7 +67,6 @@ class App extends Component {
       },
     };
 
-    // this.getCityUser();
     this.getResoures(this.state.city);
   }
 
@@ -76,19 +81,12 @@ class App extends Component {
           return {
             ...state,
             localCity: res.data.name,
-          }
+          };
         });
       },
       (err) => console.log(err)
     );
   }
-
-  // async getCityUser() {
-  //   const res = await axios.get(
-  //     `${this.apiPath}?lat=${this.state.lat}&lon=${this.state.lon}&appid=${process.env.REACT_APP_API_KEY}&lang=${this.apiLang}`
-  //   );
-  //   console.log(res.data.name);
-  // }
 
   async getResoures(city) {
     const res = await axios.get(
@@ -96,6 +94,7 @@ class App extends Component {
     );
     this.setState((state) => {
       return {
+        ...state,
         base: res.data.base,
         clouds: {
           all: res.data.clouds.all,
@@ -126,14 +125,12 @@ class App extends Component {
         },
         timezone: res.data.timezone,
         visibility: res.data.visibility,
-        weather: [
-          {
-            description: res.data.weather.description,
-            icon: res.data.weather.icon,
-            id: res.data.weather.id,
-            main: res.data.weather.main,
-          },
-        ],
+        weather: {
+          description: res.data.weather[0].description,
+          icon: res.data.weather[0].icon,
+          id: res.data.weather[0].id,
+          main: res.data.weather[0].main,
+        },
         wind: {
           deg: res.data.wind.deg,
           gust: res.data.wind.gust,
@@ -176,30 +173,119 @@ class App extends Component {
         city: state.localCity,
         localCityBool: !state.localCityBool,
         newCity: !state.newCity,
-      }
+      };
     });
     this.getResoures(this.state.city);
-  }
+  };
+
+  addActive = () => {
+    this.setState((state) => {
+      return {
+        celsius: !state.celsius,
+        fahrenheit: !state.fahrenheit,
+      };
+    });
+  };
 
   render() {
     return (
       <>
-        <input type="button" value="Сменить город" />
-        <input type="button" value="Мое местоположение" onClick={this.changedWeatherCityToLocal}/>
+        <div className="box-city">
+          <p className="city-name">{this.state.name}</p>
+          <div className="btn-group">
+            <input
+              className="change-city"
+              type="button"
+              value="Сменить город"
+            />
+            <FaLocationArrow className="icon-location" />
+            <input
+              className="local-city"
+              type="button"
+              value="Мое местоположение"
+              onClick={this.changedWeatherCityToLocal}
+            />
+          </div>
+        </div>
+
+        <div className="box-check-degrees">
+          <div className="degree-icon">&deg;</div>
+          {this.state.celsius && !this.state.fahrenheit ? (
+            <>
+              <div className="celsius active">C</div>
+              <div className="fahrenheit" onClick={this.addActive}>
+                F
+              </div>
+            </>
+          ) : (
+            <>
+              <div className="celsius" onClick={this.addActive}>
+                C
+              </div>
+              <div className="fahrenheit active">F</div>
+            </>
+          )}
+        </div>
+
         {this.state.newCity ? (
           <form onSubmit={this.updateCity}>
             <input type="text" value={this.state.city} />
             <button>Submit</button>
           </form>
         ) : (
-          <ul>
+          <ul className="list-city">
             {cities.map((item) => (
-              <li key={item.id} onClick={() => this.readCity(item.city)}>
+              <li
+                className="list-city-item"
+                key={item.id}
+                onClick={() => this.readCity(item.city)}
+              >
                 {item.city}
               </li>
             ))}
           </ul>
         )}
+
+        <div className="box-weather">
+          <p className="degrees">
+            {this.state.celsius && !this.state.fahrenheit
+              ? Math.floor(this.state.main.temp - 273.15)
+              : Math.floor(((this.state.main.temp - 273.15) * 9) / 5 + 32)}
+            &deg;
+          </p>
+          <p className="weather-description">
+            {this.state.weather.description}
+          </p>
+        </div>
+
+        <div className="list-city-info-weather">
+          <ul>
+            <li>
+              <p className="title">Ветер</p>
+              <p className="description">
+                {Math.floor(this.state.wind.speed)} м/с, западный
+              </p>
+            </li>
+            <li>
+              <p className="title">Давление</p>
+              <p className="description">
+                {Math.floor((this.state.main.pressure / 133) * 100)} мм рт.ст.
+              </p>
+            </li>
+            <li>
+              <p className="title">Влажность</p>
+              <p className="description">
+                {Math.floor(this.state.main.humidity)}%
+              </p>
+            </li>
+            <li>
+              <p className="title">Вероятность дождя</p>
+              <p className="description">
+                {Math.floor(100 - this.state.clouds.all)}%
+              </p>
+            </li>
+          </ul>
+        </div>
       </>
     );
   }
